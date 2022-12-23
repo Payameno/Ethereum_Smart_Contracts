af050fd2aa2d935280329a46ada5ee7cbf903841
Payam Beigi <beigi.payam@gmail.com>
2022-12-23 14:33:14 -0800

    Add a setter function

---
+++ Contract-Interaction/Setter.js
@@ -0,0 +1,9 @@
+/**
+ * Modify the `value` stored in the contract
+ *
+ * @param {ethers.Contract} contract - ethers.js contract instance
+ * @return {promise} a promise of transaction
+ */
+async function setValue(contract) {
+  contract.modify(1);
+}
