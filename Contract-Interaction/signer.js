dfe86d8c0e02ae1c90b025b1fc3d7c20a3092060
Payam Beigi <beigi.payam@gmail.com>
2022-12-23 15:03:33 -0800

    Change signer who performs actions on the contract

---
+++ Contract-Interaction/signer.js
@@ -0,0 +1,11 @@
+/**
+ * Set the message on the contract using the signer passed in
+ *
+ * @param {ethers.Contract} contract - ethers.js contract instance
+ * @param {ethers.types.Signer} signer - ethers.js signer instance
+ * @return {promise} a promise of transaction modifying the `message`
+ */
+async function setMessage(contract, signer) {
+  const message = "This is a new message";
+  await contract.connect(signer).modify(message);
+}
