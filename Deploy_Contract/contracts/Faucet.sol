c1eae770113cca075be0ed2b0ef43c14869a8505
Payam Beigi <beigi.payam@gmail.com>
2022-12-24 01:05:16 -0800

    Add practice, address interactions in solidity

--- Deploy_Contract/contracts/Faucet.sol
+++ Deploy_Contract/contracts/Faucet.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.0;
 
 contract Faucet {
 
+  constructor{
+    bytes addresses = [];
+  }
+
   function withdraw(uint _amount) public {
     // users can only withdraw .1 ETH at a time!
     require(_amount <= 100000000000000000);
     payable(msg.sender).transfer(_amount);
+    //push the current sender's address to the array
+    addresses.push(msg.sender);
+
   }
 
   // fallback function
