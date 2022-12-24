882c0e183b9c8058c9576600484d7c3c770f3b5c
Payam Beigi <beigi.payam@gmail.com>
2022-12-23 17:26:17 -0800

    Add network to the hardhat config

--- hardhat-practice/hardhat.config.js
+++ hardhat-practice/hardhat.config.js
@@ -4,4 +4,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      ur: process.env.ALCHEMY_TESTNET_RPC_URL,
+      accounts: process.env.TESTNET_PRIVATEKEY,
+    },
+  }
 };
