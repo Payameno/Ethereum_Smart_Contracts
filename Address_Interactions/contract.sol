051c5564e58e2568989503c8938c47b653ef3d52
Payam Beigi <beigi.payam@gmail.com>
2022-12-25 18:49:51 -0800

    Code a function to donate contract's balance to an address

--- Address_Interactions/contract.sol
+++ Address_Interactions/contract.sol
@@ -3,13 +3,19 @@ pragma solidity ^0.8.4;
 
 contract Contract {
     address public owner;
-    constructor() {
+    address public charity;
+    constructor(address _charity) {
       owner = msg.sender;
+      charity = _charity;
     }
     receive() external payable{}
 
     function tip() public payable {
-      ( bool r, ) = owner.call{value: msg.value}("");
-      require(r);
+      ( bool s, ) = owner.call{value: msg.value}("");
+      require(s);
+    }
+    function donate() public payable {
+      ( bool s, ) =  charity.call{value: address(this).balance}("");
+      require(s);
     }
 }
