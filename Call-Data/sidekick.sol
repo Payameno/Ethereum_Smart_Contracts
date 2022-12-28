42ba197c512aaadd3f5f0ef73bc975ce20b2571e
Payam Beigi <beigi.payam@gmail.com>
2022-12-27 23:43:45 -0800

    Use encodeWithSignature in order to send the call data

--- Call-Data/sidekick.sol
+++ Call-Data/sidekick.sol
@@ -1,13 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
+//encodeWithSignature will hash the function and send with variables
 contract Sidekick {
-    function sendAlert(address hero) external {
-        
-        bytes4 signature = bytes4(keccak256("alert()"));
-
-        (bool success, ) = hero.call(abi.encodePacked(signature));
-
+    function sendAlert(address hero, uint enemies, bool armed) external {
+        (bool success, ) = hero.call(
+           abi.encodeWithSignature("alert(uint256,bool)", enemies, armed)
+        );
         require(success);
     }
 }
+
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.4;
+
+// contract Sidekick {
+//     function sendAlert(address hero) external {
+        
+//         bytes4 signature = bytes4(keccak256("alert()"));
+
+//         (bool success, ) = hero.call(abi.encodePacked(signature));
+
+//         require(success);
+//     }
+// }
