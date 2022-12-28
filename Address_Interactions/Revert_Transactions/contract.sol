487a2eec03ab03d1aa5a208daf441db615ba9a37
Payam Beigi <beigi.payam@gmail.com>
2022-12-27 20:42:45 -0800

    withdraw funds and transfer to the contract owner

--- Address_Interactions/Revert_Transactions/contract.sol
+++ Address_Interactions/Revert_Transactions/contract.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.4;
 
 contract Contract {
+    address owner;
+
+//when contract depoloyed owner is the address which perform it
     constructor() payable {
+      owner = msg.sender;
       require(msg.value >= 1 ether, "Not enough ether sent");
     }
+//msg.sender here is any address that calls the function
+  function withdraw() public {
+    require(owner == msg.sender);
+    payable(msg.sender).transfer(address(this).balance);
+  }
 }
