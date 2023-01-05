7dd819fff5518a481db88dc5475158030b4a6148
Payam Beigi <beigi.payam@gmail.com>
2023-01-05 02:09:20 -0800

    Deploy the escorw contract and add 1 ether value for trasnactions

--- Escrow_Contract/Escrow.sol
+++ Escrow_Contract/Escrow.sol
@@ -5,10 +5,26 @@ contract Escrow {
     address public depositor;
     address public beneficiary;
     address public arbiter;
+    bool public isApproved;
 
     constructor(address _arbiter, address _beneficiary) payable {
       depositor = msg.sender;
       arbiter = _arbiter;
       beneficiary = _beneficiary;
+    }
+
+    event Approved(uint);
+    function approve() external {
+      //only arbiter is allowed to call this function
+      require(msg.sender == arbiter);
+
+      uint balance = address(this).balance;
+
+      //transfer contract funds to the beneficiary
+      (bool success, ) = beneficiary.call{value: address(this).balance}("");
+      require(success, "Failed to transfer funds after approval");
+
+      emit Approved(balance);
+      isApproved = true;
     }
 }
