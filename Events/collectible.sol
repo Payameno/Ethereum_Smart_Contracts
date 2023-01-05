51dc44ccaabd946b54dd35822e0c705ef2726b7e
Payam Beigi <beigi.payam@gmail.com>
2023-01-05 00:32:10 -0800

    Allow transfer of the collectible

--- Events/collectible.sol
+++ Events/collectible.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.4;
 
 contract Collectible {
   address owner;
+  uint price;
 
   event Deployed(address);
 
@@ -21,7 +22,30 @@ contract Collectible {
   event ForSale(uint, uint);
   function markPrice(uint _price) external {
     require(msg.sender == owner);
-    emit ForSale(_price, block.timestamp);
+    price = _price;
+    emit ForSale(price, block.timestamp);
   }
 
+//uint= pruchase amount/ address= buyer
+  event Purchase(uint, address);
+  function purchase() payable external {
+
+    //make sure the value is equal to asked price
+    require(msg.value == price, "1");
+
+    //make sure collectible is for sale, uint uninitialized is 0
+    require(price > 0, "2");
+
+    //before transfer, remove collectible from sale
+    price = 0;
+
+    //send the purchase price to current owner of collectible
+    (bool success, ) = owner.call{value: msg.value}("");
+    require(success);
+
+    //change owner to purchaser
+    owner = msg.sender;
+    
+    emit Purchase(msg.value, msg.sender);
+  }
 }
