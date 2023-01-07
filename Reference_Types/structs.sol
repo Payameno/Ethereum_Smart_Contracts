643d90e82443c81cf9edb30672113f8aa01ea5e1
Payam Beigi <beigi.payam@gmail.com>
2023-01-06 23:46:44 -0800

    Create struct, look up info from array of structs

--- Reference_Types/structs.sol
+++ Reference_Types/structs.sol
@@ -8,10 +8,29 @@ contract Contract {
 		Choices choice;
 		address voter;
 	}
+
+  Vote none = Vote(Choices(0), address(0));
 	
 	Vote[] public votes;
 
 	function createVote(Choices choice) external {
 		votes.push(Vote(choice, msg.sender));
 	}
+
+  function findVote(address _address) internal view returns(Vote storage){
+    for (uint i = 0; i < votes.length; i++) {
+      if (votes[i].voter == _address) {
+        return votes[i];
+      }
+      return none;
+    }
+  }
+
+  function hasVoted(address _address) external view returns(bool){
+    return findVote(_address).voter == _address;
+  }
+
+  function findChoice(address _address) external view returns(Choices){
+    return findVote(_address).choice;
+  }
 }
