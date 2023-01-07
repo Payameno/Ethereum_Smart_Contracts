5b595d1f219784883100e72c38c4de066f3da57b
Payam Beigi <beigi.payam@gmail.com>
2023-01-07 00:12:59 -0800

    Change vote

--- Reference_Types/structs.sol
+++ Reference_Types/structs.sol
@@ -13,24 +13,33 @@ contract Contract {
 	
 	Vote[] public votes;
 
-	function createVote(Choices choice) external {
+  function createVote(Choices choice) external {
+    require(!hasVoted(msg.sender));
 		votes.push(Vote(choice, msg.sender));
 	}
+
+    function changeVote(Choices _choice) external {
+    Vote storage vote = findVote(msg.sender);
+    //check if voter has indeed voted
+    require(vote.voter != none.voter);
+    vote.choice = _choice;
+    }
 
   function findVote(address _address) internal view returns(Vote storage){
     for (uint i = 0; i < votes.length; i++) {
       if (votes[i].voter == _address) {
         return votes[i];
       }
-      return none;
     }
+      return none;
   }
 
-  function hasVoted(address _address) external view returns(bool){
+  function hasVoted(address _address) public view returns(bool){
     return findVote(_address).voter == _address;
   }
 
   function findChoice(address _address) external view returns(Choices){
     return findVote(_address).choice;
   }
+
 }
